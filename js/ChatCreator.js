84b3f11051c777b89ba22510ff587fd36d5e8771
Shadic78 <carlosjosue7889@gmail.com>
2020-02-21 18:19:16 -0600

    Messages styles

--- js/ChatCreator.js
+++ js/ChatCreator.js
@@ -1,3 +1,12 @@
+document.getElementById("SelectP").addEventListener("change", function() {
+  let select = document.getElementById("SelectP");
+  let messageUser = select.options[select.selectedIndex].value;
+
+  let FormImg = document.getElementById("FormImgPersonaje");
+  FormImg.setAttribute("src", "img/" + messageUser + ".png");  
+});
+
+
 document.getElementById("BtnAddMessage").addEventListener("click", function() {
   let chatContainer = document.getElementById("ChatContainer");
   let select = document.getElementById("SelectP");
@@ -6,6 +15,7 @@ document.getElementById("BtnAddMessage").addEventListener("click", function() {
 
   let messageEstructure = "<span class='MessageUserName'>" + messageUser + "</span><br /><span class='MessageText'>" + message + "</span>";
   let messageContainer = document.createElement("div");
+  messageContainer.setAttribute("class", "MessageContainer");
   messageContainer.innerHTML = messageEstructure;
 
   let lastMessage = document.getElementById("ChatContainer").firstChild;

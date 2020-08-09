9837c6376ef498bdc9f4713cc249115e923ceefe
Shadic78 <carlosjosue7889@gmail.com>
2020-08-09 11:26:31 -0500

    Fix: Cursed mari image

--- js/Characters/Characters.js
+++ js/Characters/Characters.js
@@ -57,7 +57,7 @@ export const characters  = {
         'firstName': 'Cursed',
         'lastName': 'Mari',
         'color': '#000000',
-        'img': 'CursedMari.png'
+        'img': 'Cursed.png'
     },
     'Maki': {
         'firstName': 'Maki',
